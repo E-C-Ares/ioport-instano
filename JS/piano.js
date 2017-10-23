8dc272ee6f905d4dd435b16706445bc610dba6e3
ares <ares1899@126.com>
2017-10-23 20:01:41 +0800

    piano

--- JS/piano.js
+++ JS/piano.js
@@ -142,78 +142,47 @@
 		}
 		
 		switch(keyCode){
-			case 65:	// a
-			case 97:
-			case 81:	// q
-			case 113:
-			case 90:	// z
-			case 122:
+			case 71:  //g
 				playSound(60);
 				break;
-			case 87:	// w
-			case 119:
+			case 66:  //b
 				playSound(61);
 				break;
-			case 83:	// s
-			case 115:
-			case 88:	// x
-			case 120:
+			case 72:  //h
 				playSound(62);
 				break;
-			case 69:	// e
-			case 101:
+			case 78:  //n
 				playSound(63);
 				break;
-			case 67:	// c
-			case 99:
-			case 68:	// d
-			case 100:
+			case 74:  //j
 				playSound(64);
 				break;
-			case 70:	// f
-			case 102:
-			case 82:	// r
-			case 114:
-			case 86:	// v
-			case 118:
+			case 75:  //k
 				playSound(65);
 				break;
-			case 84:	// t
-			case 116:
+			case 44:  //,
 				playSound(66);
 				break;
-			case 66:	// b
-			case 98:
-			case 71:	// g
-			case 103:
+			case 76:  //l:
 				playSound(67);
 				break;
-			case 89:	// y
-			case 121:
+			case 46:  //.
 				playSound(68);
 				break;
-			case 72:	// h
-			case 104:
-			case 78:	// n
-			case 110:
+			case 58:  //:
+			case 59:  //;
 				playSound(69);
 				break;
-			case 85:	// u
-			case 117:
+			case 47:  //
+			case 63:  //?
 				playSound(70);
 				break;
-			case 77:	// m
-			case 109:
-			case 74:	// j
-			case 106:
+			case 34:  //"
+			case 39:  //'
 				playSound(71);
 				break;
-			case 75:	// k
-			case 107:
-			case 73:	// i
-			case 105:
-			case 44:	// ,
-			case 188:
+
+			case 100:
 				playSound(72);
 				break;
 			default:
